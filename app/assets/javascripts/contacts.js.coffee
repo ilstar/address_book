10ce300d85db1232e3414bf2949da39b80a42f75
ilstar <xingqinglzq@gmail.com>
2012-08-02 11:37:08 +0800

    list all users

--- app/assets/javascripts/contacts.js.coffee
+++ app/assets/javascripts/contacts.js.coffee
@@ -1,3 +1,9 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+# load contacts data
+$.getJSON '/contacts.json', (contacts) ->
+  $contacts = $('#contacts')
+  for contact in contacts
+    $contacts.append $("<div class='contact'><span class='name'>#{contact.name}</span></div>").attr("style", "background: url(#{contact.avatar}) no-repeat;")
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,19 +5,9 @@ class ContactsController < ApplicationController
       format.json {
         render json: [
           {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phoneNumber: '+8618611680000'}, 
-        ]
+          {name: 'Vincent Xie', avatar: "http://www.gravatar.com/avatar/7e91cf68273009a6ff7e443b9c0e6af7.png", phoneNumber: '+8618611680000'}, 
+          {name: 'Lay Zhu', avatar: "https://secure.gravatar.com/avatar/5043cfb8f89890c0fae4ea1705d92010?s=140&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png", phoneNumber: '+8618611680000'}, 
+        ] * 5
       }
     end
   end
