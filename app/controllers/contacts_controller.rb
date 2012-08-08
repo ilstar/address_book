663434d3a39a004e5e774c80f006f7857db7fcb9
Fred Liang <fred.liang@gree.co.jp>
2012-08-08 11:49:00 +0800

    stub data

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,34 @@
 class ContactsController < ApplicationController
   def index
-    respond_to do |format|
-      format.html {
-        @contacts = [
-          {name: 'Fred Liang', avatar: "http://www.gravatar.com/avatar/48986aabbdbcdeacdbc3a6dc2945a9ff.png", phone: '+8618611680001', desc: "Hey, I'm here."}, 
-          {name: 'Vincent Xie', avatar: "http://www.gravatar.com/avatar/7e91cf68273009a6ff7e443b9c0e6af7.png", phone: '+8618611680002', desc: "Don't look at me!"}, 
-          {name: 'Lay Zhu', avatar: "https://a248.e.akamai.net/assets.github.com/images/gravatars/gravatar-140.png", phone: '+8618611680003', desc: "Boo"}, 
-          {name: 'Lay Zhu', avatar: "https://a248.e.akamai.net/assets.github.com/images/gravatars/gravatar-140.png", phone: '+8618611680004', desc: "Boo"}, 
-        ] * 4 
+    @contacts = [
+      ['Andy Yao', '+8612345678', '25, Mar', 'Engineer'],
+      ['Bo Fu', '+8612345678', '6, Jun', 'Engineer'],
+      ['Carmen Tian', '+8612345678', '10, Feb', 'Engineer'],
+      ['Chang Cheng', '+8612345678', '10, Feb', 'Engineer'],
+      ['Fred Liang', '+8612345678', '10, Feb', 'Engineer'],
+      ['Jason Lai', '+8612345678', '10, Feb', 'Engineer'],
+      ['Jasper Fu', '+8612345678', '10, Feb', 'Engineer'],
+      ['Kevin Xu', '+8612345678', '10, Feb', 'Engineer'],
+      ['Lay Zhu', '+8612345678', '10, Feb', 'Engineer'],
+      ['Nemo Hu', '+8612345678', '10, Feb', 'Engineer'],
+      ['Ray Huang', '+8612345678', '10, Feb', 'Engineer'],
+      ['Rebecca Pang', '+8612345678', '10, Feb', 'Engineer'],
+      ['Richard Huang', '+8612345678', '10, Feb', 'Engineer'],
+      ['Rong Yang', '+8612345678', '10, Feb', 'Engineer'],
+      ['Tin Tian', '+8612345678', '10, Feb', 'Engineer'],
+      ['Vincent Xie', '+8612345678', '10, Feb', 'Engineer'],
+      ['XingKui Wang', '+8612345678', '10, Feb', 'Engineer']
+    ].map do |person|
+      {
+        name: person[0].split(' ').first,
+        avatar: "/assets/avatar/#{person[0].split(' ').first.downcase}.png",
+        phone: person[1],
+        desc: "#{person[3]}   #{person[2]}"
       }
+    end
+
+    respond_to do |format|
+      format.html { }
       format.json {
         render json: []
       }
